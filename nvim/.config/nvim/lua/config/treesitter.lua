c6d49344c2d947cfb2d50b01e247bc7a0c26c47f
Sam Neely <sam.neely@pm.me>
2021-06-01 11:10:06 -0400

    Ensure a bunch of treesitter configs are installed

--- nvim/.config/nvim/lua/config/treesitter.lua
+++ nvim/.config/nvim/lua/config/treesitter.lua
@@ -1,4 +1,24 @@
 require('nvim-treesitter.configs').setup {
+  ensure_installed = {
+    "bash",
+    "comment",
+    "css",
+    "dockerfile",
+    "elixir",
+    "go",
+    "gomod",
+    "graphql",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "regex",
+    "ruby",
+    "scss",
+    "tsx",
+    "typescript",
+    "yaml"
+  },
   highlight = {
     enable = true
   },
