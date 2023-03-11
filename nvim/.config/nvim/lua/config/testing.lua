ca14b9efe0b1f34742394597834b2a7fb104bcc7
Sam Neely <sam.neely@pm.me>
2023-03-11 10:05:27 -0600

    Reconfigure telescope searching

--- nvim/.config/nvim/lua/config/testing.lua
+++ nvim/.config/nvim/lua/config/testing.lua
@@ -2,6 +2,6 @@ vim.api.nvim_set_keymap('n', '<localleader>s', ":TestNearest<cr>", {noremap = tr
 vim.api.nvim_set_keymap('n', '<localleader>t', ":TestFile<cr>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<localleader>a', ":TestSuite<cr>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<localleader>l', ":TestLast<cr>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<localleader>g', ":TestVisit<cr>", {noremap = true, silent = true})
+-- vim.api.nvim_set_keymap('n', '<localleader>g', ":TestVisit<cr>", {noremap = true, silent = true})
 vim.cmd [[let g:test#javascript#runner = 'jest']]
 
