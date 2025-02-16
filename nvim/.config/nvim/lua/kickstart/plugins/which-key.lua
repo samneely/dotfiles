f101ac446233ee35dbb04baa32d5710d9c5dff5d
Sam Neely <sam.neely@proton.me>
2025-02-16 13:36:00 -0500

    Change neovim [T]oggles to T instead of t

--- nvim/.config/nvim/lua/kickstart/plugins/which-key.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/which-key.lua
@@ -64,7 +64,7 @@ return {
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
-        { '<leader>t', group = '[T]oggle' },
+        { '<leader>T', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
     },
