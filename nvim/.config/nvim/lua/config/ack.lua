0c50be12d33fb5fb0f6d8b69414760ade437c0e3
Sam Neely <sam.neely@proton.me>
2025-02-16 11:50:50 -0500

    Some initial kickstart setup + catppuccin

--- nvim/.config/nvim/lua/config/ack.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd([[
-  if executable('rg')
-    let g:ackprg = 'rg --vimgrep'
-  endif
-]])
--- nvim/.config/nvim/lua/config/formatting.lua
+++
@@ -1,4 +0,0 @@
-vim.cmd [[autocmd BufWritePre *.js Neoformat]]
-vim.cmd [[autocmd BufWritePre *.jsx Neoformat]]
-vim.cmd [[autocmd BufWritePre *.ts Neoformat]]
-vim.cmd [[autocmd BufWritePre *.tsx Neoformat]]
