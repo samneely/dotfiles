0c280bc02ef0b03cb56d9d05be7dd740cec29b02
Sam Neely <sam.neely@proton.me>
2025-02-17 15:58:35 -0500

    Change colorscheme to catppuccin-frappe

--- nvim/.config/nvim/lua/kickstart/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/colorscheme.lua
@@ -8,7 +8,7 @@ return {
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      vim.cmd.colorscheme 'catppuccin-macchiato'
+      vim.cmd.colorscheme 'catppuccin-frappe'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
