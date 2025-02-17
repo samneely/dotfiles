6991cc7c1477edefcea3f726f881f0d5114400c4
Sam Neely <sam.neely@proton.me>
2025-02-17 15:26:19 -0500

    Set the tab settings in neovim

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -61,4 +61,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Tab settings
+vim.opt.tabstop = 2
+vim.opt.sts = 2
+vim.opt.expandtab = true
+
 -- vim: ts=2 sts=2 sw=2 et
